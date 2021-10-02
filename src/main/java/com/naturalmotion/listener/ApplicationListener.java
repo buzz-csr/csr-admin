0e556c6533c56694f18f863f707342df6d258480
Jerome <jerome.desachy@gmail.com>
2021-10-02 15:51:22 +0200

    Add chris1 team

--- src/main/java/com/naturalmotion/listener/ApplicationListener.java
+++ src/main/java/com/naturalmotion/listener/ApplicationListener.java
@@ -5,17 +5,23 @@ import javax.servlet.ServletContextListener;
 
 public class ApplicationListener implements ServletContextListener {
 
+	private Thread threadChrisMembers;
 	private Thread threadRedMembers;
 	private Thread threadOrangeMembers;
+	private Thread threadChrisCrew;
 	private Thread threadRedCrew;
 	private Thread threadOrangeCrew;
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		threadChrisMembers = new Thread(new AccountHistoryTask("rouge"));
+		threadChrisMembers.start();
 		threadRedMembers = new Thread(new AccountHistoryTask("rouge"));
 		threadRedMembers.start();
 		threadOrangeMembers = new Thread(new AccountHistoryTask("orange"));
 		threadOrangeMembers.start();
+		threadChrisCrew = new Thread(new CrewHistoryTask("rouge"));
+		threadChrisCrew.start();
 		threadRedCrew = new Thread(new CrewHistoryTask("rouge"));
 		threadRedCrew.start();
 		threadOrangeCrew = new Thread(new CrewHistoryTask("orange"));
@@ -24,6 +30,8 @@ public class ApplicationListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
+		threadChrisMembers.interrupt();
+		threadChrisCrew.interrupt();
 		threadRedMembers.interrupt();
 		threadOrangeMembers.interrupt();
 		threadRedCrew.interrupt();
