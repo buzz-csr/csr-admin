40ab53f1b8a18c7799bbbb1c9237e7c897075660
Jerome <jerome.desachy@gmail.com>
2021-10-16 10:39:58 +0200

    Ajout log hook

--- src/main/java/com/naturalmotion/servlet/rest/LineWebHook.java
+++ src/main/java/com/naturalmotion/servlet/rest/LineWebHook.java
@@ -5,13 +5,18 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
+
 @Path("/line")
 public class LineWebHook {
+
+	private Logger log = Logger.getLogger(LineWebHook.class);
 
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
 	@Path("/hook")
 	public String sayPlainTextHello(String content) {
+		log.info("Received Line Hook: \n" + content);
 		return "{ \"receivedHook\": \"" + content + "\" }";
 	}
 }
