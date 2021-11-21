c203cc7e8232b334b4ef8bd0fec57bf59c9ac940
Jerome <jerome.desachy@gmail.com>
2021-11-21 14:21:02 +0100

    Correction Database init

--- src/main/java/com/naturalmotion/database/DatabaseInitializer.java
+++ src/main/java/com/naturalmotion/database/DatabaseInitializer.java
@@ -8,14 +8,14 @@ public class DatabaseInitializer {
 
 	private static final String CREATE_TOKEN_TABLE = "CREATE TABLE IF NOT EXISTS TOKEN(crew VARCHAR2(20),"
 	        + "rarity VARCHAR(20),status VARCHAR2(20),paid INTEGER,cost INTEGER);";
-	private static final String CREATE_USER_TOKEN_TABLE = "CREATE TABLE IF NOT EXISTS USER_TOKEN(id VARCHAR2(20),"
-	        + "user VARCHAR(20),rarity VARCHAR2(20),paid INTEGER);";
+//	private static final String CREATE_USER_TOKEN_TABLE = "CREATE TABLE IF NOT EXISTS USER_TOKEN(id VARCHAR2(20),"
+//	        + "user VARCHAR(20),rarity VARCHAR2(20),paid INTEGER);";
 
 	public void init() throws SQLException {
 		try (Connection connection = ConnectionFactory.create();) {
 			try (Statement statement = connection.createStatement();) {
 				statement.executeUpdate(CREATE_TOKEN_TABLE);
-				statement.executeUpdate(CREATE_USER_TOKEN_TABLE);
+//				statement.executeUpdate(CREATE_USER_TOKEN_TABLE);
 			}
 			connection.commit();
 		}
