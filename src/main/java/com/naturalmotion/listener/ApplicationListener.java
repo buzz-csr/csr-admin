92cb4761c3499a69ad6d019e76d140048877c5bb
Jerome <jerome.desachy@gmail.com>
2021-11-21 14:22:36 +0100

    Nettoyage teams

--- src/main/java/com/naturalmotion/listener/ApplicationListener.java
+++ src/main/java/com/naturalmotion/listener/ApplicationListener.java
@@ -11,12 +11,8 @@ import com.naturalmotion.database.DatabaseInitializer;
 
 public class ApplicationListener implements ServletContextListener {
 
-	private Thread threadChrisMembers;
 	private Thread threadRedMembers;
-//	private Thread threadOrangeMembers;
-	private Thread threadChrisCrew;
 	private Thread threadRedCrew;
-//	private Thread threadOrangeCrew;
 
 	private Thread eventThread;
 	private EventTask eventTask;
@@ -30,18 +26,10 @@ public class ApplicationListener implements ServletContextListener {
 			eventThread = new Thread(eventTask);
 			eventThread.start();
 
-			threadChrisMembers = new Thread(new AccountHistoryTask("chris"));
-			threadChrisMembers.start();
 			threadRedMembers = new Thread(new AccountHistoryTask("rouge"));
 			threadRedMembers.start();
-//			threadOrangeMembers = new Thread(new AccountHistoryTask("orange"));
-//			threadOrangeMembers.start();
-			threadChrisCrew = new Thread(new CrewHistoryTask("chris"));
-			threadChrisCrew.start();
 			threadRedCrew = new Thread(new CrewHistoryTask("rouge"));
 			threadRedCrew.start();
-//			threadOrangeCrew = new Thread(new CrewHistoryTask("orange"));
-//			threadOrangeCrew.start();
 
 		} catch (SQLException e) {
 			Logger.getLogger(ApplicationListener.class).error("Error initializing database");
@@ -51,12 +39,8 @@ public class ApplicationListener implements ServletContextListener {
 
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
-		threadChrisMembers.interrupt();
-		threadChrisCrew.interrupt();
 		threadRedMembers.interrupt();
 		threadRedCrew.interrupt();
-//		threadOrangeMembers.interrupt();
-//		threadOrangeCrew.interrupt();
 
 		if (eventTask != null) {
 			eventTask.stop();
