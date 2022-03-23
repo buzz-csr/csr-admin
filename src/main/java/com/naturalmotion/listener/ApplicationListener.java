3b9f1505c22d2ec0442a715ffc2b199c20cc451c
Jerome <jerome.desachy@gmail.com>
2022-03-23 08:03:48 +0100

    remove session auto update

--- src/main/java/com/naturalmotion/listener/ApplicationListener.java
+++ src/main/java/com/naturalmotion/listener/ApplicationListener.java
@@ -21,7 +21,6 @@ public class ApplicationListener implements ServletContextListener {
 	private TokenTask tokenTask;
 	private AccountHistoryTask accountHistoryTask;
 	private CrewHistoryTask crewHistoryTask;
-	private SessionUpdateTask session;
 
 	private Server server;
 
@@ -45,9 +44,6 @@ public class ApplicationListener implements ServletContextListener {
 
 			crewHistoryTask = new CrewHistoryTask();
 			crewHistoryTask.start();
-
-			session = new SessionUpdateTask();
-			session.start();
 
 			Runtime.getRuntime().addShutdownHook(new Thread() {
 
